48028fbbd5a4680afb7a72b25adbfb3d54d7245e
sam <sammy.k.mutua@gmail.com>
2023-01-31 15:08:55 +0300

    Adding a User notification to notify them when they are warned by the authorites

---
+++ app/Notifications/Domains/User/BullyFlagNotitication.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Notifications\Domains\User;
+
+use Domains\Shared\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+
+class BullyFlagNotitication extends Notification {
+    use Queueable;
+
+    public function __construct(
+        public User $authority
+    ) {
+    }
+
+    public function via($notifiable): array {
+        return ['database'];
+    }
+
+    public function toArray($notifiable): array {
+        return [
+            'flag_count' => $notifiable->bully_flags,
+            'message' => "You have been warned by the authority in accordance to your resent 
+                behaver towards another user of our community.Further warning will lead to automatic blocking and further investigations from us.",
+            'authority' => [
+                'name' => $this->authority->name,
+                'email' => $this->authority->email
+            ]
+        ];
+    }
+}
